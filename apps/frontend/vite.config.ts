c9d18c092685ba7a7cb803c8c6c41491aff40ed8
stefvw93 <stef@stefvanwijchen.com>
2026-08-17 09:19:09 +0200

    add tauri

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  pack: {},
-  lint: {
-    options: {
-      typeAware: true,
-      typeCheck: true,
+  server: {
+    watch: {
+      ignored: ["**/src-tauri/**"],
     },
   },
-  fmt: {},
-  test: {},
 });
