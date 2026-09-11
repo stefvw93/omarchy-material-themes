6e18f2eaf30669a4c2372e568abfd3c943c4189c
Stef van Wijchen <stefvanwijchen@gmail.com>
2026-09-11 13:47:27 +0200

    layout tweak

--- apps/frontend/src/features/seed/index.tsx
+++ apps/frontend/src/features/seed/index.tsx
@@ -259,8 +259,8 @@ const reducer = SeedDefinition.reducer({
 });
 
 const render = SeedDefinition.render(({ state, dispatch }) => (
-  <div className="grid grid-cols-12 flex-1 min-h-0 gap-2 p-2">
-    <div className="col-span-6 xl:col-span-8 flex flex-col flex-1 min-h-0 gap-2">
+  <div className="grid grid-cols-12 flex-1 min-h-0 gap-2 p-2 min-w-2xl">
+    <div className="col-span-5 sm:col-span-6 xl:col-span-8 flex flex-col flex-1 min-h-0 gap-2">
       <Tabs
         value={state.inputType}
         onValueChange={(value) => dispatch(SetInputKind.make({ inputType: value }))}
@@ -293,7 +293,7 @@ const render = SeedDefinition.render(({ state, dispatch }) => (
       </Tabs>
     </div>
 
-    <div className="col-span-6 xl:col-span-4 h-full" id="output">
+    <div className="col-span-7 sm:col-span-6 xl:col-span-4 h-full" id="output">
       <OutputPanel />
     </div>
   </div>
